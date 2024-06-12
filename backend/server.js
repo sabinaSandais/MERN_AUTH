db723fd083ffc34ccadceac3f95eb72746ae1563
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-06-12 12:01:28 +0200

    set the express app and dependancies for backend

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const port = 5000;
+const app = express();
+app.get('/', (req, res ) => res.send('Server is ready'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
