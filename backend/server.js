99e551c0ec256a767da225e42aee1c5a9574d725
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-06-18 15:08:17 +0200

    created .env and gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
-const port = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5000;
 const app = express();
 app.get('/', (req, res ) => res.send('Server is ready'));
 app.listen(port, () => console.log(`Server running on port ${port}`));
