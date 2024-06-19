3e8ebc4c25dbe91890f65c9068ff289095712890
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-06-19 20:14:07 +0200

    async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
+import asyncHandler from 'express-async-handler'
 //@desc auth user/set token
 //routePost /api/users/auth
 //@access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Auth User'});
-};
+});
 
     export {
         authUser
